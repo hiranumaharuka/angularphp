15511f04043da6d01e94ccbc42091b17c3b6d9b7
hiranumaharuka <colorcord251@gmail.com>
2020-07-07 20:05:22 +0900

    fix server index

--- server.js
+++ server.js
@@ -2,9 +2,11 @@ const express = require('express');
 const path = require('path');
 const app = express();
 app.use(express.static(__dirname + '/dist/ng9-crud'));
-app.get('/*', function(req,res) {
-  res.sendFile(path.join(__dirname+
-    '/dist/ng9-crud/index.html'));});
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname + '/dist/ng9-crud/index.html'));
+});
+var distDir = __dirname + '/dist/';
+app.use(express.static(distDir));
 // app.get('/*', function(req, res) {
 //   res.sendFile('index.html', {root: 'dist/ng9-crud/'}
 // );
