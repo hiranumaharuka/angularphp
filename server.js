e5f77619171b0c297486f06bf19950f2e91a6701
hiranumaharuka <colorcord251@gmail.com>
2020-07-07 20:00:11 +0900

    fix server

--- server.js
+++ server.js
@@ -2,12 +2,12 @@ const express = require('express');
 const path = require('path');
 const app = express();
 app.use(express.static(__dirname + '/dist/ng9-crud'));
-// app.get('/*', function(req,res) {
-//   res.sendFile(path.join(__dirname+
-//     '/dist/index.html'));});
-app.get('/*', function(req, res) {
-  res.sendFile('index.html', {root: 'dist/ng9-crud/'}
-);
-});
+app.get('/*', function(req,res) {
+  res.sendFile(path.join(__dirname+
+    '/dist/ng9-crud/index.html'));});
+// app.get('/*', function(req, res) {
+//   res.sendFile('index.html', {root: 'dist/ng9-crud/'}
+// );
+// });
 app.listen(process.env.PORT || 8080);
 console.log('Server listening on port 8080');
