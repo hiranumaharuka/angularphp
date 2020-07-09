34b4e31c3c0dcc139e557e7b22b9dc6b621820c3
hiranumaharuka <colorcord251@gmail.com>
2020-07-09 13:04:59 +0900

    index.phpに変更

--- web_api/view.php
+++ web_api/view.php
@@ -3,10 +3,10 @@ header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: PUT, GET, POST");
 header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
 // include database connection
-include 'config/database.php';
- 
+include 'config/index.php';
+
 // delete message prompt will be here
- 
+
 // select all data
 $query = "SELECT p_id, p_name, p_description, p_price FROM products ORDER BY p_id DESC";
 $stmt = $con->prepare($query);
