8e083eaa1e194b4a217be6046d67f9c37c9eec86
hiranumaharuka <colorcord251@gmail.com>
2020-07-10 15:11:31 +0900

    create変更

--- create.php
+++ create.php
@@ -2,37 +2,36 @@
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: PUT, GET, POST");
 header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
-if($_POST){
+if ($_POST) {
 
 // include database connection
-include 'index.php';
+    include 'index.php';
 
-try{
+    try {
 
 // insert query
-$query = "INSERT INTO posts SET title=:title, content:content, category=:category, authorId:authorId";
-// prepare query for execution
-$stmt = $con->prepare($query);
-// posted values
-$title = $_POST['title'];
-$content = $_POST['content'];
-$category = $_POST['category'];
-$authorId = $_POST['authorId'];
-// bind the parameters
-$stmt->bindParam(':title', $title);
-$stmt->bindParam(':content', $content);
-$stmt->bindParam(':category', $category);
-$stmt->bindParam(':authorId', $authorId);
-// Execute the query
-if($stmt->execute()){
-    echo json_encode(array('result'=>'success'));
-}else{
-    echo json_encode(array('result'=>'fail'));
+        $query = "INSERT INTO posts SET title=:title, content:content, category=:category, authorId:authorId";
+        // prepare query for execution
+        $stmt = $db->prepare($query);
+        // posted values
+        $title = $_POST['title'];
+        $content = $_POST['content'];
+        $category = $_POST['category'];
+        $authorId = $_POST['authorId'];
+        // bind the parameters
+        $stmt->bindParam(':title', $title);
+        $stmt->bindParam(':content', $content);
+        $stmt->bindParam(':category', $category);
+        $stmt->bindParam(':authorId', $authorId);
+        // Execute the query
+        if ($stmt->execute()) {
+            echo json_encode(array('result'=>'success'));
+        } else {
+            echo json_encode(array('result'=>'fail'));
+        }
+    }
+    // show error
+    catch (PDOException $exception) {
+        die('ERROR: ' . $exception->getMessage());
+    }
 }
-}
-// show error
-catch(PDOException $exception){
-die('ERROR: ' . $exception->getMessage());
-}
-}
-?>
