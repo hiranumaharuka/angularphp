b8d22b37f454cfba71aff6db5556c5a4ae1702b0
hiranumaharuka <colorcord251@gmail.com>
2020-07-10 14:23:30 +0900

    php変更

--- view.php
+++ view.php
@@ -6,12 +6,29 @@ header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Ac
 include 'index.php';
 
 // delete message prompt will be here
-
+error_reporting(E_ERROR);
 // select all data
-$query = "SELECT * FROM posts ORDER BY postId DESC";
-$stmt = $con->prepare($query);
-$stmt->execute();
-$results = $stmt->fetchAll(PDO::FETCH_ASSOC);
-$json = json_encode($results);
-echo $json;
+$query = "SELECT * FROM posts";
+$stmt = $db->prepare($query);
+$posts = [];
+if($stmt->execute())
+{
+  $cr = 0;
+ while($row = $stmt->fetch(PDO::FETCH_ASSOC))
+ {
+  $users[$cr]['postId']    = $row['postId'];
+  $users[$cr]['title'] = $row['title'];
+  $users[$cr]['content'] = $row['content'];
+  $users[$cr]['category'] = $row['category'];
+  $users[$cr]['authorId'] = $row['authorId'];
+  $cr++;
+ }
+
+ echo json_encode($posts);
+}
+// $stmt->execute();
+// $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+// $json = json_encode($results);
+// echo $json;
 ?>
+
